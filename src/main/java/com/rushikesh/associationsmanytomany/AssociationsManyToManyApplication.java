c36c57ddf5286e653baaed4aa548211356c145d0
rushikesh123 <rushi@gmail.com>
2023-02-25 13:21:19 +0530

    Added MainApp

--- src/main/java/com/rushikesh/associationsmanytomany/AssociationsManyToManyApplication.java
+++ src/main/java/com/rushikesh/associationsmanytomany/AssociationsManyToManyApplication.java
@@ -2,6 +2,10 @@ package com.rushikesh.associationsmanytomany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.rushikesh.associationsmanytomany.entity.Programmer;
+import com.rushikesh.associationsmanytomany.repository.ProgrammerRepository;
 
 @SpringBootApplication
 public class AssociationsManyToManyApplication {
@@ -11,7 +15,9 @@ public class AssociationsManyToManyApplication {
 	
 	ProgrammerRepository repo   =	context.getBean(ProgrammerRepository.class);
 
-
+     Programmer p = new Programmer();
+     p.setName("Sujata");
+     p.setSalary(450000000);
 		
 	}
 
